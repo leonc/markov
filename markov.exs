5924b53e50354ac632ceeddacc40d2b79cdff8c0
Leon T Chism II <leon@chism.org>
2014-08-06 21:38:30 -0500

    fixing some small bugs

--- markov.exs
+++ markov.exs
@@ -1,29 +1,17 @@
 defmodule Markov do
 
   defstruct markov_map: HashDict.new()
-  def new() do
-    HashDict.new()
-  end
 
   # TODO: should I use atoms here? Does it matter?
   # __START__
   # __END__
-
-  # a global hash, where the first word
-  # is the key, and value is an array of some lenght with
-  markov_map = HashDict.new()
-
-  # reset the database of word pairs
-  def init_map do
-    markov_map = HashDict.new()
-  end # def init_map
 
   def add_text(text) do
     add_words(["__START__"|String.split(text)])
   end # def add_text
 
   
-  def add_words(words) when length words == 1 do
+  def add_words(words) when (length words) == 1 do
     [first|_] = words
     add_pair(first,"__END__")
   end
@@ -42,18 +30,6 @@ defmodule Markov do
   def dump do
     IO.puts markov_map
   end
-
-  # #########################################################################
-  # not good code at all
-  # #########################################################################
-  def add_pair_old(first, second) do
-    case Dict.fetch(markov_map, first) do
-      # the case where "first" isn't already known to the database
-      :error -> markov_map = Dict.put(markov_map,first,[second])
-      # the case where it was already in there
-      {:ok, following_words} -> markov_map = Dict.put(markov_map,first,[second|following_words])
-    end
-  end # def add_paid
 
 end # the Markov module declaration
 
